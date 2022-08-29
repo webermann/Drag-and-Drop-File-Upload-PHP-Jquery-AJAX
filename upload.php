10f7a8ea26af2b92510ec94f66484938feafa4da
Hauke Webermann <hauke@webermann.net>
2022-08-29 14:31:31 +0200

    Changed coding style

--- upload.php
+++ upload.php
@@ -1,15 +1,15 @@
 <?php
 header('Content-Type: application/json');
 $uploaded = array();
-if(!empty($_FILES['file']['name'][0])){
-	foreach($_FILES['file']['name'] as $position => $name){
-		if(move_uploaded_file($_FILES['file']['tmp_name'][$position], '../upload/' . $name)){
-			$uploaded[] = array(
-				'name' =>$name,
-				'file' => '/upload/' . $name
-				);
-		}		
-	}
+if (!empty($_FILES['file']['name'][0])) {
+    foreach ($_FILES['file']['name'] as $position => $name) {
+        if (move_uploaded_file($_FILES['file']['tmp_name'][$position], '../upload/' . $name)) {
+            $uploaded[] = array(
+                'name' =>$name,
+                'file' => '/upload/' . $name
+            );
+        }        
+    }
 }
 
 echo json_encode($uploaded);
