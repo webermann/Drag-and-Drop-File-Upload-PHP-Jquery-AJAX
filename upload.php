c0d5b0c4808db7bc75dc5fe53e0c5f27592da339
Hauke Webermann <hauke@webermann.net>
2022-08-29 13:57:40 +0200

    Changed path to upload in root

--- upload.php
+++ upload.php
@@ -3,10 +3,10 @@ header('Content-Type: application/json');
 $uploaded = array();
 if(!empty($_FILES['file']['name'][0])){
 	foreach($_FILES['file']['name'] as $position => $name){
-		if(move_uploaded_file($_FILES['file']['tmp_name'][$position], 'uploads/' . $name)){
+		if(move_uploaded_file($_FILES['file']['tmp_name'][$position], '../upload/' . $name)){
 			$uploaded[] = array(
 				'name' =>$name,
-				'file' => 'uploads/' . $name
+				'file' => '/upload/' . $name
 				);
 		}		
 	}
